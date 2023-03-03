c6f617b12cf7e68b3b24c8057e78c4e3dd2f4696
cobsccomp192p-014 <35591927+hishd@users.noreply.github.com>
2023-03-03 17:36:49 +0000

    changed InnerHTML to firstChild.nodeValue

--- Week 4/e5.js
+++ Week 4/e5.js
@@ -1,7 +1,7 @@
 document.onclick = function() {
     const h1 = document.getElementById('heading1');
-    h1.innerHTML = 'This is the updated heading 1 by javascript.';
+    h1.firstChild.nodeValue = 'This is the updated heading 1 by javascript.';
 
     const p1 = document.getElementById('paragraph1');
-    p1.innerHTML = 'This is the updated paragraph by javascript.';
+    p1.firstChild.nodeValue = 'This is the updated paragraph by javascript.';
 }
