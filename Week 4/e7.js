338cd0ffc4f1718507745e2be63ad094f3030e5a
cobsccomp192p-014 <35591927+hishd@users.noreply.github.com>
2023-03-09 19:16:37 +0000

    Added onKeyup event

--- Week 4/e7.js
+++ Week 4/e7.js
@@ -1,15 +1,6 @@
-const button1 = document.getElementById('button1');
 const input1 = document.getElementById('input1');
-const alertBox = document.querySelector('.alert-box');
-alertBox.style.display = 'none';
-
-button1.onclick = () => {
-    // alert(`You Entered : ${input1.value}`)
-    if(alertBox.style.display === 'none'){
-        alertBox.style.display = 'block';
-        console.log("Visible")
-    }else{
-        alertBox.style.display = 'none';
-        console.log("Hidden")
-    }
+const text = document.getElementById('text');
+input1.onkeyup = function() {
+  console.log(input1.value);
+  text.firstChild.nodeValue = input1.value;
 }
