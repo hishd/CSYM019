aea7ccbc62b71d6df90eb4402c8a0b4bff6e137c
cobsccomp192p-014 <35591927+hishd@users.noreply.github.com>
2023-03-09 19:11:52 +0000

    Added alert box insted of JS alert

--- Week 4/e7.js
+++ Week 4/e7.js
@@ -1,5 +1,15 @@
 const button1 = document.getElementById('button1');
 const input1 = document.getElementById('input1');
+const alertBox = document.querySelector('.alert-box');
+alertBox.style.display = 'none';
+
 button1.onclick = () => {
-    alert(`You Entered : ${input1.value}`)
+    // alert(`You Entered : ${input1.value}`)
+    if(alertBox.style.display === 'none'){
+        alertBox.style.display = 'block';
+        console.log("Visible")
+    }else{
+        alertBox.style.display = 'none';
+        console.log("Hidden")
+    }
 }
